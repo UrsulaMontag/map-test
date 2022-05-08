579c711529722090540d46f5bc11961ccd9dbf23
Ursula Montag <montagu666@gmail.com>
2022-05-08 17:58:27 +0200

    Add access-token

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -12,8 +12,8 @@ const Map = () => {
       style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/umontag/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidW1vbnRhZyIsImEiOiJjbDJ4aDZveGMwdnEwM2tvejlkaDY1NGdrIn0.YEz0XFlyfegL-mHmn6b-SQ`}
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
       <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
         <Popup>Hey ! you found me</Popup>
