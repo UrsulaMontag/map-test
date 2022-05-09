8e51f1789d0388dd58f9c9706796adb124979a9e
Ursula Montag <montagu666@gmail.com>
2022-05-09 14:35:54 +0200

    delete wrong comma

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -9,15 +9,17 @@ const Map = () => {
       center={[40.8054, -74.0241]}
       zoom={14}
       scrollWheelZoom={false}
-      style={{ height: "100%", width: "100%" }}
+      style={{ height: 400, width: "100%" }}
     >
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/umontag/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidW1vbnRhZyIsImEiOiJjbDJ4aDZveGMwdnEwM2tvejlkaDY1NGdrIn0.YEz0XFlyfegL-mHmn6b-SQ`}
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+        // url={`https://api.mapbox.com/styles/v1/umontag/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidW1vbnRhZyIsImEiOiJjbDJ4aDZveGMwdnEwM2tvejlkaDY1NGdrIn0.YEz0XFlyfegL-mHmn6b-SQ`}
+        // attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
+      {/* <Marker position={[51.505, -0.09]}>
         <Popup>Hey ! you found me</Popup>
-      </Marker>
+      </Marker> */}
     </MapContainer>
   );
 };
